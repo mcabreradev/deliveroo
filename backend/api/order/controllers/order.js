110f214af5fdc333ddf8dd79b396944a942bb9b8
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2020-07-13 12:00:33 -0300

    update banckend

--- backend/api/order/controllers/order.js
+++ backend/api/order/controllers/order.js
@@ -1,8 +1,44 @@
 'use strict';
+const stripe = require('stripe')('sk_test_fy1bGjkGhzCPRSJEG5SipWKQ');
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  create: async ctx => {
+    const {
+      address,
+      amount,
+      dishes,
+      postalCode,
+      token,
+      city,
+    } = ctx.request.body;
 
-module.exports = {};
+    // Charge the customer
+    try {
+      await stripe.charges.create({
+        // Transform cents to dollars.
+        amount: amount * 100,
+        currency: 'usd',
+        description: `Order ${new Date()} by ${ctx.state.user.id}`,
+        source: token,
+      });
+
+      // Register the order in the database
+      try {
+        const order = await strapi.services.order.create({
+          user: ctx.state.user.id,
+          address,
+          amount,
+          dishes,
+          postalCode,
+          city,
+        });
+
+        return order;
+      } catch (err) {
+        // Silent
+      }
+    } catch (err) {
+      // Silent
+    }
+  },
+};
